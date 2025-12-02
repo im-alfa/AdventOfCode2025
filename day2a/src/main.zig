d54f5844a68caa06ade01ee5506e92c2c9e58c63
alfa <alfamodz26@gmail.com>
2025-12-02 20:39:10 +0000

    feat: impl day2

---
+++ day2a/src/main.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+const input = @embedFile("input.txt");
+//const input = @embedFile("my-input.txt");
+
+const MAX_SPINS: i64 = 100;
+
+pub fn main() !void {
+    var ranges = std.mem.tokenizeAny(u8, input, ",\n");
+    var sum: u64 = 0;
+
+    while (ranges.next()) |r| {
+        const pos = std.mem.indexOf(u8, r, "-") orelse continue;
+        const start_str = r[0..pos];
+        const end_str = r[pos + 1 ..];
+
+        const start = try std.fmt.parseInt(u64, start_str, 10);
+        const end = try std.fmt.parseInt(u64, end_str, 10);
+
+        const nstart = @as(u64, @intFromFloat(@ceil(@log10(@as(f64, @floatFromInt(start))))));
+        const nend = @as(u64, @intFromFloat(@ceil(@log10(@as(f64, @floatFromInt(end))))));
+
+        if (nstart % 2 != 0 and nend % 2 != 0) continue;
+
+        //std.debug.print("{} ({}) -- {} ({})\n", .{ start, nstart, end, nend });
+        for (start..end + 1) |idx| {
+            const n = @as(u64, @intFromFloat(@ceil(@log10(@as(f64, @floatFromInt(idx))))));
+            if (@mod(n, 2) != 0) continue;
+
+            const divisor = std.math.pow(u64, 10, n / 2);
+            const left_half = idx / divisor;
+            const right_half = idx % divisor;
+
+            if (left_half == right_half) {
+                sum += idx;
+            }
+        }
+    }
+
+    std.debug.print("Sum: {}\n", .{sum});
+}
---
+++ day2b/src/main.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+const input = @embedFile("input.txt");
+//const input = @embedFile("my-input.txt");
+
+const MAX_SPINS: i64 = 100;
+
+fn isInvalidId(n: u64) bool {
+    var buf: [20]u8 = undefined;
+    const s = std.fmt.bufPrint(&buf, "{}", .{n}) catch unreachable;
+
+    if (s.len <= 1) return false;
+
+    const ns = s.len;
+    var doubled: [40]u8 = undefined;
+    @memcpy(doubled[0..ns], s);
+    @memcpy(doubled[ns .. ns * 2], s);
+
+    const mid = doubled[1 .. s.len * 2 - 1];
+    return std.mem.indexOf(u8, mid, s) != null;
+}
+
+pub fn main() !void {
+    var ranges = std.mem.tokenizeAny(u8, input, ",\n");
+    var sum: u64 = 0;
+
+    while (ranges.next()) |r| {
+        const pos = std.mem.indexOf(u8, r, "-") orelse continue;
+        const start = try std.fmt.parseInt(u64, r[0..pos], 10);
+        const end = try std.fmt.parseInt(u64, r[pos + 1 ..], 10);
+
+        var idx = start;
+        while (idx <= end) : (idx += 1) {
+            if (isInvalidId(idx)) sum += idx;
+        }
+    }
+
+    std.debug.print("Sum: {}\n", .{sum});
+}
